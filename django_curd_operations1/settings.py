9f464b0e4750a4b5b127acfa6c30a07840241442
ajaymiryala2002 <ajaymiryala259@gmail.com>
2025-11-17 21:02:50 +0530

    Added requirements and Procfile for Render deployment

--- django_curd_operations1/settings.py
+++ django_curd_operations1/settings.py
@@ -74,13 +74,23 @@ WSGI_APPLICATION = 'django_curd_operations1.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.mysql",
+        "NAME": "django_curd_deployee",
+        "USER": "root",
+        "PASSWORD": "Ajay@2002",
+        "HOST": "127.0.0.1",
+        "PORT": "3306",
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
